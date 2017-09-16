1a7bc6465dfd0f3f68d25e4984d0839fec4644ed
Tuscann <fbinnzhivko@gmail.com>
2017-09-16 15:57:40 +0300

    Add new staff

--- 04.C# BASIC SYNTAX - EXERCISES/src/ivan/_04_Beverage_Labels.java
+++ 04.C# BASIC SYNTAX - EXERCISES/src/ivan/_04_Beverage_Labels.java
@@ -1,29 +1,24 @@
 package ivan;
 
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class _04_Beverage_Labels {   // 90/100
+public class _04_Beverage_Labels {   // 100/100
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        double volume = Double.parseDouble(scanner.nextLine());
+        double energy = Double.parseDouble(scanner.nextLine());
+        double sugar = Double.parseDouble(scanner.nextLine());
+        double totalEnergy = energy * (volume/100);
+        double totalSugar = sugar * (volume/100);
 
-        String label = input.nextLine();
-        Integer volume = Integer.parseInt(input.nextLine());
-        Integer energyContent = Integer.parseInt(input.nextLine());
-        Integer sugar = Integer.parseInt(input.nextLine());
+        DecimalFormat myFormatter = new DecimalFormat("###.####");
 
-        Double energyPerContent = (double) energyContent * volume / 100;
-        Double sugar_needed = (double) sugar * volume / 100;
 
-        String x = energyPerContent.toString();
-
-        System.out.printf("%dml %s:\n", volume, label);
-        if (x.contains(".")) {
-            BigDecimal stripedVal = new BigDecimal(x).stripTrailingZeros();
-            System.out.printf("%skcal, %sg sugars", stripedVal, sugar_needed.toString());
-        } else {
-            System.out.printf("%skcal, %sg sugars", energyPerContent, sugar_needed.toString());
-        }
+        System.out.printf("%sml %s:%n", myFormatter.format(volume),name);
+        System.out.printf("%skcal, %sg sugars",
+                myFormatter.format(totalEnergy) , myFormatter.format(totalSugar));
     }
 }
 
---
+++ 05.C# CONDITIONAL STATEMENTS AND LOOPS/src/ivan/_12_Number_Checker2.java
@@ -0,0 +1,17 @@
+package ivan;
+import java.util.Scanner;
+
+public class _12_Number_Checker2 {
+    Scanner scanner = new Scanner(System.in);
+
+        for(;;)    {
+        try {
+            int number = Integer.parseInt(scanner.nextLine());
+            System.out.println("It is a number.");
+            break;
+        } catch (Exception ex) {
+            System.out.println("Invalid input!");
+            break;
+        }
+    }
+}
