ab984f21c8f7edfce1932d8a284e8bdc84f2a1cb
Tuscann <fbinnzhivko@gmail.com>
2017-09-25 23:49:01 +0300

    Finish String Processing - Lab

--- 05.C# CONDITIONAL STATEMENTS AND LOOPS/src/ivan/_12_Number_Checker2.java
+++ 05.C# CONDITIONAL STATEMENTS AND LOOPS/src/ivan/_12_Number_Checker2.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class _12_Number_Checker2 {
     Scanner scanner = new Scanner(System.in);
 
-        for(;;)    {
-        try {
-            int number = Integer.parseInt(scanner.nextLine());
-            System.out.println("It is a number.");
-            break;
-        } catch (Exception ex) {
-            System.out.println("Invalid input!");
-            break;
-        }
-    }
+//        for(;;)    {
+//        try {
+//            int number = Integer.parseInt(scanner.nextLine());
+//            System.out.println("It is a number.");
+//            break;
+//        } catch (Exception ex) {
+//            System.out.println("Invalid input!");
+//            break;
+//      }
+//    }
 }
